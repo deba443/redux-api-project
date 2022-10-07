0537090f9f3018fa5214feffcd03916ae8658f13
Deba <debaprasad443@gmail.com>
2022-10-07 18:25:09 +0530

    started the crud operation on photos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import Main from './components/Main'
-import { Routes,Route,BrowserRouter } from 'react-router-dom';
+import { Routes,Route} from 'react-router-dom';
 import Welcome from './components/Welcome'
-// import Dummy from './components/Dummy';
 import Photos from './components/Photos'
+import CreateModal from './components/CreateModal';
 function App() {
   return (
     <div className="App ">
-      {/* <BrowserRouter> */}  
+ 
       <Routes>
         <Route path="/" element={<Welcome/>}/>
         <Route path="user" element={<Main/>}/>
         <Route path="user/:id/photos" element={<Photos/>}/>
       </Routes>
-      {/* <Photos/> */}
-      {/* </BrowserRouter> */}
+      {/* <CreateModal/> */}
       
     </div>
   );
--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,26 +1,50 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import CreateModal from "./CreateModal";
+// import { useParams } from "react-router-dom";
 
 const Photos = (props) => {
-    const { photos, posts, ide } = useSelector((state) => state.post)
-    // const id=useParams()
     let data;
+    const { photos, posts, ide } = useSelector((state) => state.post)
+    const[open,setOpen]=useState(false)
+    // const getData = () => {
+    //     let list = JSON.parse(localStorage.getItem("photosData"));
+    //     if (list.length) {
+    //         return JSON.parse(localStorage.getItem("photosData"))
+    //     }
+    //     else {
+    //         return [];
+    //     }
+    // }
+    // const [data, setData] = useState([{ide:getData()}])
     if (photos.length) {
         data = photos.find((item) => item.id === ide)
 
     }
-    // console.log(data)
+    const openCreateModal=()=>setOpen(true);
+    const closeCreatModal=()=>setOpen(false)
+    // useEffect(() => {
+    //     localStorage.setItem('photosData', JSON.stringify(data))
+    // }, [data])
     return (
         <div className="MY-AUTO mx-auto">
-            {/* <h1>Hey</h1> */}
-            <div className="max-w-xs max-h-fit mx-auto my-auto"><img src={data.url} alt="" /></div>
-
-            <div className="max-w-xs mx-auto my-auto">
-                <button>update</button>
-                <button>create</button>
-                <button>Delete</button>
-                {/* <button>delete</button> */}
+            {open && <CreateModal open={openCreateModal} close={closeCreatModal}/>}
+            <div className="flex flex-wrap flex-col justify-center items-center gap-5 max-w-5xl mx-auto my-auto">
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    openCreateModal()
+                }}>Create</button>
+                <div>
+                    <div className="max-w-xs max-h-fit mx-auto my-auto">
+                        <img src={data.url} alt="" />
+                        <div className="max-w-xs mx-auto my-auto">
+                            <button>update</button>
+                            <button>Delete</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
